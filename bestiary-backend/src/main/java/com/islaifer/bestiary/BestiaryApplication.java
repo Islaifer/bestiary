aea394120771393d464aeae266c2ede5b81037c7
islaifer23 <islaifer2379@gmail.com>
2022-10-07 11:29:15 -0300

    first commit

---
+++ bestiary-backend/src/main/java/com/islaifer/bestiary/BestiaryApplication.java
@@ -0,0 +1,13 @@
+package com.islaifer.bestiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestiaryApplication.class, args);
+    }
+
+}
---
+++ bestiary-backend/src/test/java/com/islaifer/bestiary/BestiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.islaifer.bestiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
